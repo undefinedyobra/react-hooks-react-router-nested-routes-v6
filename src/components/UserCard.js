95b27e5305ad973cd42f1658082377f57fa1a501
undefinedyobra <brianmelly008@gmail.com>
2024-05-01 11:48:08 +0300

    Complete lab

--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -1,12 +1,15 @@
 import { Link } from "react-router-dom";
 
-function UserCard({user}) {
-    return (
-      <article>
-          <h2>{user.name}</h2>
-          <Link to={`/profile/${user.id}`}>View profile</Link>
-      </article>
-    );
-  };
-  
-  export default UserCard;
+function UserCard({ user }) {
+  return (
+    <article>
+      <h2>{user.name}</h2>
+      <p>
+        {/* Link to the profile using user.id */}
+        <Link to={`/profile/${user.id}`}>View profile</Link>
+      </p>
+    </article>
+  );
+}
+
+export default UserCard;
--- src/routes.js
+++ src/routes.js
@@ -1,30 +1,32 @@
+import App from "./App";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Login from "./pages/Login";
 import UserProfile from "./pages/UserProfile";
 import ErrorPage from "./pages/ErrorPage";
-
 const routes = [
-  {
-    path: "/",
-    element: <Home />,
-    errorElement: <ErrorPage />
-  }, 
-  {
-    path: "/about",
-    element: <About />,
-    errorElement: <ErrorPage />
-  },
-  {
-    path: "/login",
-    element: <Login />,
-    errorElement: <ErrorPage />
-  },
-  {
-    path: "/profile/:id",
-    element: <UserProfile />,
-    errorElement: <ErrorPage />
-  }
+    {
+        path: "/",
+        element: <App />,
+        errorElement: <ErrorPage />,
+        children: [
+             {
+                path: "/",
+                element: <Home />
+            }, 
+            {
+                path: "/about",
+                element: <About />
+            },
+            {
+                path: "/login",
+                element: <Login />
+            },
+            {
+                path: "/profile/:id",
+                element: <UserProfile />
+            }
+        ]
+    }
 ];
-
 export default routes;
